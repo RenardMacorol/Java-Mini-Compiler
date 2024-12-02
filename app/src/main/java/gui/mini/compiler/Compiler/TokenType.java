7f1785578cefc30fa599a3006e691d17f3e7f4c4
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2024-12-02 23:37:53 +0800

    Update TokenType.java with BOOLEAN_LITERAL

--- app/src/main/java/gui/mini/compiler/Compiler/TokenType.java
+++ app/src/main/java/gui/mini/compiler/Compiler/TokenType.java
@@ -1,17 +1,16 @@
 package gui.mini.compiler.Compiler;
 
-enum TokenType {
+public enum TokenType {
     // Single Character Tokens
     SEMICOLON, DOT, COMMA,
     ADD, SUB, MUL, DIV, ASSIGN_OP,
 
     // Literal
-    INDENTIFIER, STRING, NUMBER, CHAR,
+    INDENTIFIER, STRING, NUMBER, CHAR, BOOLEAN_LITERAL,
 
     // Base Type
     INT, STRING_TYPE, CHAR_TYPE, FLOAT, DOUBLE, BOOLEAN,
 
     // End of the file
     EOF
-
 }
